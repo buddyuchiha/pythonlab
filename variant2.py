3f6a6fbdf58f89780b8fc447d44c2d53318ece1d
afrodetka <Pro100biv@mail.ru>
2023-12-06 06:57:22 +0400

    commit 2

--- variant2.py
+++ variant2.py
@@ -2,12 +2,16 @@ from bs4 import BeautifulSoup
 import requests 
 import csv
 from time import sleep
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.5845.686 YaBrowser/23.9.5.686 Yowser/2.5 Safari/537.36"
+  }
 
-url = "https://www.gismeteo.ru/diary/4618/2022/1/"
-response = requests.get(url)
+url = "https://www.gismeteo.ru/diary/4618/2023/11/"
+response = requests.get(url, headers = headers)
 soup = BeautifulSoup(response.text, "lxml")
-table = soup.find("table", allign = "center")
-string = soup.find("tr", allign = "center")
-temperature = soup.find("td", class_ = "first_in_group")
-pressure = soup.find("td")
-wind = soup.find("span")
+table = soup.find("table", align = "center")
+lines = table.find_all("tr", align = "center")
+for line in lines:
+    information = line.find_all("td")
+    for i in information:
+        print(i.text)
